38d19d1dab4a2725a7f12f92625b6e4b7394edd9
cs4alhaider <cs.alhaider@gmail.com>
2019-05-05 00:44:48 +0300

    Initial Commit

---
+++ SetupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetupProject
+//
+//  Created by Abdullah Alhaider on 05/05/2019.
+//  Copyright © 2019 Abdullah Alhaider - https://github.com/cs4alhaider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
